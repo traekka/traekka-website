7a152798a7461cb02b4c8d261e76187a6648a557
Maximilian Hoheiser <max.hoheiser@gmail.com>
2023-03-11 22:13:21 +0100

    fix: Lint-staged configrc throwing errors
    replace .lintstagedrc with lint-staged.config.js fixes eslint errors
    caused by not ignoring paths stated in .eslintirc

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write", "eslint"],
+  "**/*.ts?(x)": () => "npm run check-types",
+  "*.{json,yaml,yml,md}": ["prettier --ignore-unknown --write"],
+  "*.{css,scss,less}": ["stylelint --fix", "prettier --write"],
+  "*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged",
+};
