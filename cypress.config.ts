ce63b174023317bfacc0197d3b3c3858e31185e2
Maximilian Hoheiser <max.hoheiser@gmail.com>
2023-03-12 12:14:09 +0100

    feat: Feat: Add cypress configuration

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
