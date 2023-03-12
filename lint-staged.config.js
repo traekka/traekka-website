dad9bebccedee23d2514275b7705e04eafbdfaf4
Maximilian Hoheiser <max.hoheiser@gmail.com>
2023-03-12 21:10:20 +0100

    fix: Type check cypress with correct config file
    type checking of primarily fixtures from cypress should be performed
    with extended tsconfig file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write", "eslint"],
   "**/*.ts?(x)": () => "tsc-files --noEmit --pretty",
+  "cypress/**/*.ts": () => "tsc-files --project cypress --noEmit --pretty",
   "*.{json,yaml,yml,md}": ["prettier --ignore-unknown --write"],
   "*.{css,scss,less}": ["stylelint --fix", "prettier --write"],
   "*.{png,jpeg,jpg,gif,svg}": "imagemin-lint-staged",
