cb889015f4e47509e8f76c222f7964dbc45fd4b7
Maximilian Hoheiser <max.hoheiser@gmail.com>
2023-03-13 10:26:45 +0100

    fix: Update e2e test for landingpage

--- cypress/e2e/landingpage.cy.ts
+++ cypress/e2e/landingpage.cy.ts
@@ -3,6 +3,6 @@ describe("Landingpage", () => {
     cy.visit("http://localhost:3000/");
   });
   it("should display landingpage header", () => {
-    cy.findAllByText("Hi").should("be.visible");
+    cy.findAllByText("Hi you are on the test homepage").should("be.visible");
   });
 });
