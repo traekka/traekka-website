3200cf336c8fe23ef1a23b51de21c06f0af78756
Maximilian Hoheiser <max.hoheiser@gmail.com>
2023-03-13 15:14:31 +0100

    feat: Add percy snapshot to example e2e test

--- cypress/e2e/landingpage.cy.ts
+++ cypress/e2e/landingpage.cy.ts
@@ -1,8 +1,15 @@
 describe("Landingpage", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
   });
   it("should display landingpage header", () => {
     cy.findAllByText("Hi you are on the test homepage").should("be.visible");
+  });
+  it("should take screenshot of the landingpage", () => {
+    cy.findByRole("heading", {
+      name: "Hi you are on the test homepage",
+    }).should("be.visible");
+
+    cy.percySnapshot("Landingpage");
   });
 });
